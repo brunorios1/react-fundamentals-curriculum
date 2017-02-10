ce339d51d268aeab307bb5db2834417d74406762
Bruno Rios <brunorios1@gmail.com>
2017-02-10 17:45:51 -0200

    adds UI for header

--- app/components/GetWeatherForm.js
+++ app/components/GetWeatherForm.js
@@ -3,10 +3,10 @@ var ReactDOM = require('react-dom');
 
 function GetWeatherForm (props) {
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="St. George, Utah"/>
-        <button type="button">Get Weather</button>
+    <div className="get-weather-form">
+      <form className="get-weather-form__form form-inline">
+        <input className="form-control" type="text" placeholder="St. George, Utah"/>
+        <button type="button" className="btn btn-success">Get Weather</button>
       </form>
     </div>
   )
--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -1,12 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var HelloWorld = require('../components/HelloWorld');
+var GetWeatherForm = require('../components/GetWeatherForm');
 
 var MainContainer = React.createClass({
   render: function () {
     return (
       <div>
-        <div className="header"></div>
+        <div className="header">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-12 col-md-6">
+                <div className="logo">My Weather App</div>
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <GetWeatherForm />
+              </div>
+            </div>
+          </div>
+        </div>
+
         {this.props.children}
       </div>
     )

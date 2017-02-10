e2253ae79c3865226ce6592118495cb907106c66
Bruno Rios <brunorios1@gmail.com>
2017-02-09 22:03:00 -0200

    adds an hello world component.

---
+++ app/components/HelloWorld.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function HelloWorld (props) {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+module.exports = HelloWorld;
